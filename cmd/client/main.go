2d16dd1a05db9a6f5da329a8680ad9bda152f71d
Benjamin Lopez <contact@scotow.com>
2018-10-24 20:30:54 +0200

    [WIP] tcp connection buffering

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -1,7 +1,74 @@
 package main
 
-import "fmt"
+import (
+	"bytes"
+	"io"
+	"io/ioutil"
+	"log"
+	"net"
+	"net/http"
+	"time"
+)
+
+var (
+//conn *net.TCPConn
+//buffer *bytes.Buffer
+)
+
+func fetchData() {
+	time.Sleep(time.Millisecond * 1000)
+	for {
+		resp, err := http.Get("http://localhost:8080/")
+		if err != nil {
+			log.Panicln(err)
+		}
+
+		//io.Copy(conn, resp.Body)
+		resp.Body.Close()
+		time.Sleep(time.Millisecond * 2000)
+	}
+}
+
+func sendData(buffer *bytes.Buffer) {
+	for {
+		/*fmt.Println(buffer.Len())
+		_, err := http.Post("http://localhost:8080/", "application/octet-stream", &buffer)
+		if err != nil {
+			log.Panicln(err)
+		}
+		fmt.Println(buffer.Len())
+
+		fmt.Println("Sending data over HTTP")*/
+
+		time.Sleep(time.Millisecond * 2000)
+		log.Println("Before copy", buffer.Len())
+		io.Copy(ioutil.Discard, buffer)
+		log.Println("After copy", buffer.Len())
+	}
+}
 
 func main() {
-	fmt.Println("hello")
+	addr, err := net.ResolveTCPAddr("tcp", ":2222")
+	if err != nil {
+		log.Panic(err)
+	}
+
+	listener, err := net.ListenTCP("tcp", addr)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	c, err := listener.AcceptTCP()
+	if err != nil {
+		log.Panic(err)
+	}
+
+	var buffer bytes.Buffer
+
+	//conn = c
+
+	//go fetchData()
+	go sendData(&buffer)
+
+	io.Copy(&buffer, c)
 }
--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,86 +1,54 @@
 package main
 
 import (
-	"github.com/gorilla/websocket"
+	"bytes"
+	"github.com/gorilla/mux"
 	"io"
 	"log"
 	"net"
 	"net/http"
 )
 
-var upgrader = websocket.Upgrader{
-	ReadBufferSize:  1024,
-	WriteBufferSize: 1024,
+var (
+	buffer  bytes.Buffer
+	sshConn *net.TCPConn
+)
+
+func initSsh() {
+	addr, err := net.ResolveTCPAddr("tcp4", "127.0.0.1:5555")
+	if err != nil {
+		log.Panic(err)
+	}
+
+	conn, err := net.DialTCP("tcp4", nil, addr)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	sshConn = conn
+	io.Copy(&buffer, sshConn)
 }
 
-func newSshConn() (*net.TCPConn, error) {
-	addr, err := net.ResolveTCPAddr("tcp4", "127.0.0.1:2222")
-	if err != nil {
-		return nil, err
-	}
-
-	sshConn, err := net.DialTCP("tcp4", nil, addr)
-	if err != nil {
-		return nil, err
-	}
-
-	return sshConn, nil
+func handleOutput(w http.ResponseWriter, r *http.Request) {
+	io.Copy(w, &buffer)
 }
 
-func handleWsConn(w http.ResponseWriter, r *http.Request) {
-	sshConn, err := newSshConn()
-	if err != nil {
-		log.Println("cannot open ssh connection", err)
-		return
-	}
-
-	wsConn, err := upgrader.Upgrade(w, r, nil)
-	if err != nil {
-		log.Println(err)
-		return
-	}
-
-	go func() {
-		for {
-			messageType, reader, err := wsConn.NextReader()
-			if err != err {
-				log.Println(err)
-				continue
-			}
-
-			log.Printf("Type: %d\n", messageType)
-			io.Copy(sshConn, reader)
-		}
-	}()
-
-	for {
-		writer, err := wsConn.NextWriter(websocket.BinaryMessage)
-		if err != err {
-			log.Println(err)
-			continue
-		}
-
-		log.Printf("New writter\n")
-		io.Copy(writer, sshConn)
-		writer.Close()
-	}
-}
-
-func serveHome(w http.ResponseWriter, r *http.Request) {
-	log.Println(r.URL)
-	if r.URL.Path != "/" {
-		http.Error(w, "Not found", http.StatusNotFound)
-		return
-	}
-	if r.Method != "GET" {
-		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
-		return
-	}
-	http.ServeFile(w, r, "html/index.html")
+func handleInput(w http.ResponseWriter, r *http.Request) {
+	log.Println("Handling input....")
+	defer r.Body.Close()
+	io.Copy(sshConn, r.Body)
 }
 
 func main() {
-	http.HandleFunc("/", serveHome)
-	http.HandleFunc("/ws", handleWsConn)
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	// Listen on /comment for HTTP post
+	// Send data received on /comment to stdout
+	// On each HTTP get request on /video sends buffer
+
+	go initSsh()
+
+	r := mux.NewRouter()
+	r.HandleFunc("/", handleOutput).Methods("GET")
+	r.HandleFunc("/", handleInput).Methods("POST")
+	http.Handle("/", r)
+	log.Fatal(http.ListenAndServe(":8080", r))
 }
