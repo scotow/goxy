dff1e7b4875a75b9c8986dcd20c6816246a3ab62
Benjamin Lopez <contact@scotow.com>
2018-11-06 20:44:58 +0100

    [WIP] chuncked response

---
+++ test/long_response/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+)
+
+func handleSlow(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "sending first line of data")
+	if f, ok := w.(http.Flusher); ok {
+		f.Flush()
+	} else {
+		log.Println("Damn, no flush")
+	}
+	time.Sleep(3 * time.Second)
+	fmt.Fprintln(w, "sending second line of data")
+}
+
+func handleFast(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "sending first line of data")
+	fmt.Fprintln(w, "sending second line of data")
+}
+
+func main() {
+	http.HandleFunc("/", handleFast)
+	http.HandleFunc("/slow", handleSlow)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
